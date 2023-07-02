26f0b3566817379cc8b41541ecbbb503e968d60f
Sanskar-J <sanskar16.jaiswal@gmail.com>
2023-07-02 16:21:44 +0530

    Add project files.

---
+++ Face Det/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Face_Det
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+                choDevice.Items.Add(device.Name);
+
+            choDevice.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[choDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        private void Device_NewFrame(object sender,NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = new Image<Bgr,byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2 , 1);
+            foreach(Rectangle rectangle in rectangles)
+            {
+                using(Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using(Pen pen = new Pen(Color.Red, 1))
+                    {
+                        graphics.DrawRectangle(pen, rectangle);
+                    }
+                }
+            }
+            Bitmap resizedBitmap = new Bitmap(bitmap, pictureBox1.Size);
+
+            // Dispose the original bitmap
+            bitmap.Dispose();
+
+            // Display the resized bitmap in the PictureBox
+            pictureBox1.Image = resizedBitmap;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (device.IsRunning)
+                device.Stop();
+        }
+    }
+}

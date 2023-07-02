02ea3408e69a51e615eefd57855ffe02cac263cc
Sanskar-J <sanskar16.jaiswal@gmail.com>
2023-07-02 19:45:02 +0530

    message

--- Face Det/Form1.Designer.cs
+++ Face Det/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(637, 98);
+            this.button1.Location = new System.Drawing.Point(641, 204);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(68, 67);
             this.button1.TabIndex = 0;
